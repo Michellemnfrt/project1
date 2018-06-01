267665cdec0610690f6c3720097ad43611c26d1b
Michelle Monfort <Michellemnfrt@gmail.com>
2018-06-01 11:22:24 -0400

    update markup, start on js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-var cards = document.querySelectorAll('.graphic')
+// 1. add function to all cards so that the click does something
+// duplicate the alternative photos for the face of the cards
+// 2. change picture when clicked (create function to do this)
+// 3. keep first clicked picture displayed
+// 4. compare second picture to first picture
+// 5. if there is a match make them dissapear
+// 6. if there is no match make them flip back over
+// 7. repeat steps 5 and 6 until there are no cards left
+// 8. display game over
 
-for (let pics = 0; pics < cards.length; pics++) {
-// let photo = cards[pics];
-// photo.addEventListener('click',function() {console.log ("snot");
-  cards[pics].addEventListener('click', changeImage)
-}
-function changeImage () { 
-  var back = document.getElementsById("1")
+var cards = document.querySelectorAll(".cell"){
 
-  console.log(back.src)
-  console.log(this.children[0])
-}
-// var newCards = document.querySelectorAll('.glass-stack')
-//document.getElementById('.graphic').childNodes;
-//cards.document[pics].
+for (i = 0; i < cards.length; i++)
+cards.[i]
