b40cb64ff50f15b3aad4dde3bee9062c6d62c35c
Michelle Monfort <Michellemnfrt@gmail.com>
2018-07-13 20:45:30 -0400

    install lodash

--- vidtut.js
+++ vidtut.js
@@ -12,8 +12,74 @@ tiles_flipped =0;
 memory_array = _.shuffle(memory_array);
 var output ='';
 _.forEach(memory_array, function(memory_array_value, index) {
-    output +='<div id= "tile_';
-    
+    output += '<div id= "tile_' + index + '" onclick="memoryFlipTile(this,\''+ memory_array_value +'\')'></div>';  
 });
+document.getElementById('memory_board').innerHTML = output;
+}
 
+function memoryFlipTile(tile, value) {
+    if (canFlipCard(tile)) {
+        flipCard(tile, value);
+        if (areNoCardsFlipped()) {
+            setCardsAsFlipped(tile, value);
+        } else if(isOneCardFlipped()) {
+            setCardsAsFlipped(tile, value);
+            if(isThereIsAMatch()) {
+                matchCards();
+                if (isGameOver()) {
+                    gameIsOver();
+                }
+            } else {
+                cardsDoNotMatch();
+            }
+        }
+    }
+}
+if(tile.innerHTML =="" && memory_values.length < 2)
+
+function canFlipCard(tile) {
+return tile.innerHTML == " " && memory_values.length < 2; 
+}
+
+function flipCard(tile, value) {
+    tile.style.background = 'FFF';
+    tile.innerHTML = value;
+}
+function areNoCardsFlipped() {
+    return memory_values.length == 0;
+}
+function isOneCardFlipped() {
+    return memory_values.length == 1;
+}
+if(memory_values.length == 0) {
+    memory_values.push(val);
+    memory_tile_ids.push(tile.id);
+}
+
+function isThereIsAMatch() {
+    return memory_values[0] == memory_values[1];
+}
+function matchCards () {
+    tiles_flipped += 2;
+    memory_values = [];
+    memory_tiles_ids = [];
+}
+
+
+function isGameOver() {
+    return tiles_flipped == memory_array.length;
+}
+function cardsDoNotMatch() {
+    setTimeout(flipCardBack, 700);
+}
+
+function flipCardBack() {
+    var tile_1 = document.getElementById(memory_tile_ids[0]);
+    var tile_2 = document.getElementById(memory_tile_ids[1]);
+    tile_1.style.background = '#FF3399';
+    tile_1.innerHTML = " ";
+    tile_2.style.background = '#FF3399';
+    tile_2.innerHTML = " ";
+    memory_values = [];
+    memory_tile_ids = [];
 }
